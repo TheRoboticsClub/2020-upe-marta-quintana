c746807425884399c654edda6865a91e5f32af7f
Marta Quintana <markinpor@hotmail.com>
2020-12-22 10:26:23 +0100

    Arreglando error consola todo ok editor mayor height

--- FollowLine New web-template/assets/birds_eye.js
+++ FollowLine New web-template/assets/birds_eye.js
@@ -6,19 +6,19 @@ var mapCanvas = document.getElementById("birds-eye"),
 // Given the coordinates of center, and
 // the angle towards which it points
 function drawCircle(x, y){
-	ctx.clearRect(0, 0, mapCanvas.width, mapCanvas.height);	
-	
+	ctx.clearRect(0, 0, mapCanvas.width, mapCanvas.height);
+
 	cursor_x = x;
 	cursor_y = y;
-	
+
 	ctx.beginPath();
 	ctx.arc(x, y, 2, 0, 2 * Math.PI);
 	ctx.closePath();
-	
+
 	ctx.lineWidth = 1.5;
 	ctx.strokeStyle = "#666666";
 	ctx.stroke();
-	
+
 	ctx.fillStyle = "#FF0000";
 	ctx.fill();
 }
